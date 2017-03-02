b006338ede62be843b3cc7e051b2f7c4ecbe1794
Ben Hamilton <benhamilton10@gmail.com>
2017-03-02 20:41:35 +0000

    First commit

---
+++ desktop/src/ca/polygone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ca.polygone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.polygone.PolyGone;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PolyGone(), config);
+	}
+}
