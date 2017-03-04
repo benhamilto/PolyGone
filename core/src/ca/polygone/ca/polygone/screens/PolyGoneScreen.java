4c569108a1bc8b9c9f4304416bbafe75ba9cff0b
Ben Hamilton <benhamilton10@gmail.com>
2017-03-04 22:27:21 +0000

    menuscreen with a button

---
+++ core/src/ca/polygone/ca/polygone/screens/PolyGoneScreen.java
@@ -0,0 +1,7 @@
+package ca.polygone.ca.polygone.screens;
+
+/**
+ * Created by bhamilto on 3/4/17.
+ */
+public class PolyGoneScreen {
+}
---
+++ core/src/ca/polygone/ca/polygone/screens/TestScreen.java
@@ -0,0 +1,7 @@
+package ca.polygone.ca.polygone.screens;
+
+/**
+ * Created by bhamilto on 3/3/17.
+ */
+public class TestScreen {
+}
