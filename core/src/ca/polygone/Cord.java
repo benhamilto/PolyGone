61baa29c5a638cd138592847ccc9ee34c8ef09c3
chuggajohn <jab550@mun.ca>
2017-03-05 12:16:43 -0330

    forgot to add some files

---
+++ core/src/ca/polygone/Cord.java
@@ -0,0 +1,7 @@
+package ca.polygone;
+
+/**
+ * Created by John on 2017-03-05.
+ */
+public class Cord {
+}
---
+++ core/src/ca/polygone/Environment.java
@@ -0,0 +1,9 @@
+package ca.polygone;
+
+import java.util.HashMap;
+/**
+ * Created by John on 2017-03-05.
+ */
+public class Environment {
+        private HashMap<Cord,Piece>Map
+}
