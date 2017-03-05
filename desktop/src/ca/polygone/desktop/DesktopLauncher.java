abd508fa77686e39cbc9e73c3661b1f1a8e292e6
Ben Hamilton <benhamilton10@gmail.com>
2017-03-05 18:00:54 +0000

    Forgot to add some files

--- desktop/src/ca/polygone/desktop/DesktopLauncher.java
+++ desktop/src/ca/polygone/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import ca.polygone.PolyGone;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1000;
+		config.height = 563;
 		new LwjglApplication(new PolyGone(), config);
 	}
 }
