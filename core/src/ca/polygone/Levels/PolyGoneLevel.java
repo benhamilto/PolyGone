670c2177560af86c71ceaff86af63ad19e65064a
Ben Hamilton <Ben Hamilton>
2017-03-29 20:37:18 -0230

    setting up levels, does not work correctly

---
+++ core/src/ca/polygone/Levels/PolyGoneLevel.java
@@ -0,0 +1,7 @@
+package ca.polygone.Levels;
+
+/**
+ * Created by Ben on 2017-03-29.
+ */
+public class PolyGoneLevel {
+}
--- core/src/ca/polygone/WallVert.java
+++ core/src/ca/polygone/WallVert.java
@@ -3,5 +3,5 @@ package ca.polygone;
 /**
  * Created by Ben on 2017-03-29.
  */
-public class LevelLoader {
+public class WallVert {
 }
